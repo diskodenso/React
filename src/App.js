cfdf906355f90c0d5052ab2644eb5e22751d457f
Denis Olf <denisadnanolf@gmail.com>
2021-05-06 09:39:32 +0200

    Images and flipping Cards, React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from "./components/Home.js";
+import Home from "./Components/Home.js";
+import Card from "./Components/Card.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Rick And Morty</h1>
       <Home></Home>
+      <Card></Card>
     </div>
   )
 }
